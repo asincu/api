31e6543f9e498c02c00d94f59b3dc8ea15045978
Alina Militaru <asincu@users.noreply.github.com>
2021-08-25 19:44:49 -0700

    Add nullable

--- pkg/apis/projectcalico/v3/packetcapture.go
+++ pkg/apis/projectcalico/v3/packetcapture.go
@@ -75,6 +75,7 @@ type PacketCaptureSpec struct {
 	// If omitted or the value is in the past, the capture will start immediately.
 	// If the value is changed to a future time, capture will stop immediately and restart at that time
     // +optional
+    // +nullable
 	// +kubebuilder:validation:Format="date-time"
 	StartTime *metav1.Time `json:"startTime,omitempty" validate:"omitempty"`
 
@@ -82,6 +83,7 @@ type PacketCaptureSpec struct {
 	// If omitted the capture will continue indefinitely.
 	// If the value is changed to the past, capture will stop immediately.
 	// +optional
+	// +nullable
 	//+kubebuilder:validation:Format="date-time"
 	EndTime *metav1.Time `json:"endTime,omitempty" validate:"omitempty"`
 }
