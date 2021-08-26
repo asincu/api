282e4b9db401495b042a2363fddf452378180fd0
Alina Militaru <asincu@users.noreply.github.com>
2021-08-26 15:04:30 -0700

    Drop pointers

--- pkg/apis/projectcalico/v3/packetcapture.go
+++ pkg/apis/projectcalico/v3/packetcapture.go
@@ -75,13 +75,13 @@ type PacketCaptureSpec struct {
 	// If omitted or the value is in the past, the capture will start immediately.
 	// If the value is changed to a future time, capture will stop immediately and restart at that time
 	// +optional
-	StartTime *metav1.Time `json:"startTime,omitempty"`
+	StartTime metav1.Time `json:"startTime,omitempty"`
 
 	// Defines the end time at which this PacketCapture will stop capturing packets.
 	// If omitted the capture will continue indefinitely.
 	// If the value is changed to the past, capture will stop immediately.
 	// +optional
-	EndTime *metav1.Time `json:"endTime,omitempty"`
+	EndTime metav1.Time `json:"endTime,omitempty"`
 }
 
 // A PacketCaptureRule encapsulates a set of match criteria for traffic captured from an interface.
--- pkg/openapi/openapi_generated.go
+++ pkg/openapi/openapi_generated.go
@@ -7385,12 +7385,14 @@ func schema_pkg_apis_projectcalico_v3_PacketCaptureSpec(ref common.ReferenceCall
 					"startTime": {
 						SchemaProps: spec.SchemaProps{
 							Description: "Defines the start time from which this PacketCapture will capture packets. If omitted or the value is in the past, the capture will start immediately. If the value is changed to a future time, capture will stop immediately and restart at that time",
+							Default:     map[string]interface{}{},
 							Ref:         ref("k8s.io/apimachinery/pkg/apis/meta/v1.Time"),
 						},
 					},
 					"endTime": {
 						SchemaProps: spec.SchemaProps{
 							Description: "Defines the end time at which this PacketCapture will stop capturing packets. If omitted the capture will continue indefinitely. If the value is changed to the past, capture will stop immediately.",
+							Default:     map[string]interface{}{},
 							Ref:         ref("k8s.io/apimachinery/pkg/apis/meta/v1.Time"),
 						},
 					},
